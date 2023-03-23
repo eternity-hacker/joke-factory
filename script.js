18f733da6fbad4628f0125636b0161f74b1d3a13
Olivia <eternity@hotlookbook.com>
2023-03-23 15:09:34 -0700

    refactored inserting categories into its own module

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
 import getJokeInfo from "./modules/jokeInfo.js"
-const figuresBox = document.querySelector("#figures-box")
+import insertCategories from "./modules/insertCategories.js"
+
+
+
 getJokeInfo()
-  .then(function (jokeInfo) {
-    //we need to display the supported categories
-    jokeInfo.categories.forEach(function (element, index, array) {
-      console.log(element)
-      if (element === "Any") {
-        return null
-      }
-      //create a figure element.
-      const figure = document.createElement("figure")
-      //create three children elements, which will be contained in figure. Img, h3, div
-      const img = document.createElement("img");
-      const h3 = document.createElement("h3");
-      const div = document.createElement("div");
-      // Add attributes or and/or content to the children
-      img.src = `./images/categories/${element}.png`
-      h3.textContent = `${element}`
-      //insert afterbegin to figure with the three children elements inside of it.
-      figure.insertAdjacentElement("beforeend", img)
-      figure.insertAdjacentElement("beforeend", h3)
-      figure.insertAdjacentElement("beforeend", div)
-      figuresBox.insertAdjacentElement("beforeend", figure)
-
-
-    })
-  })
+  .then(insertCategories)
